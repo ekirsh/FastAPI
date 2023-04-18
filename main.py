d994a43aa7d8f09761cc2dd6cf0d4f0bce8eac54
Ezra Kirsh <ezkirsh@gmail.com>
2023-04-18 04:16:14 -0400

    requests 6

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ headers = {
 def scrape_artist(artist_name):
     url = f"https://genius.com/artists/{artist_name.replace(' ', '-')}"
 
-    response = requests.get(url, proxies=proxy, headers=headers)
+    response = requests.get(url, proxies=proxy, headers=headers, verify=False)
     soup = BeautifulSoup(response.content, 'html.parser')
     print(soup)
     song_list = soup.find_all('div', class_='mini_card-title')
